4790506dc75085cbd2f52f2183ddfc23d5e59970
yukyu30 <fukanoyugo+dev@gmail.com>
2021-01-26 12:01:34 +0900

    モバイル向けに最適化

--- src/components/Works/Modal.vue
+++ src/components/Works/Modal.vue
@@ -3,11 +3,11 @@
         <div class="overflow-x-hidden overflow-y-auto text-blue-700 fixed inset-0 z-40 outline-none focus:outline-none justify-center items-center flex">
             <div class="relative w-auto my-6  max-w-3xl mx-1 md:mx-0">
                 <!--content-->
-                <div class=" h-screen-80 border-0  rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                <div class="  md:h-screen-80 border-0  rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
                     <!--header-->
-                    <div class="flex h-20 items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
+                    <div class="flex md:h-20 items-start justify-between p-5 border-b border-solid border-gray-300 rounded-t">
                         <h3 class="text-3xl font-semibold">{{ work.title }}</h3>
-                        <button class="h-8 w-8 overflow-hidden stroke-current text-red-500 bg-transparent border border-solid border-red-500 hover:border-gray-500 hover:text-gray-500 active:bg-red-600 font-bold text-sm rounded outline-none focus:outline-none" type="button" v-on:click="closeModal">
+                        <button class="md:-8 w-8 overflow-hidden stroke-current text-red-500 bg-transparent border border-solid border-red-500 hover:border-gray-500 hover:text-gray-500 active:bg-red-600 font-bold text-sm rounded outline-none focus:outline-none" type="button" v-on:click="closeModal">
                             <svg class="stroke" width="100%" height="100%" viewBox="0 0 15 15" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/"><path d="M0,0l15,15"/><path d="M15,0l-15,15" /></svg>
                         </button>
                     </div>
@@ -28,7 +28,7 @@
                         
                     </div>
                     <!--footer-->
-                    <div class="p-6 h-20 border-gray-300 rounded-b">
+                    <div class="p-6 md:h-20 border-gray-300 rounded-b">
                         <p class="tag" v-for="category in work.categories"  :key="category.index">{{category}}</p>                   
                     </div>
                 </div>
