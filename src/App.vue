70c64c062d277088da289cc9d26c8f55961f35b4
yukyu30 <fukanoyugo+dev@gmail.com>
2021-02-01 23:33:21 +0900

    p5.jsを導入

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="bg-white w-screen h-screen flex flex-row flex-wrap p-3">
-    <div class="w-full m-2 md:mx-auto md:w-2/3">
+  
+  <div class="w-full h-full flex flex-row flex-wrap" > 
+    <div class="overflow-hidden z-0 fixed" id="canvas"></div>
+    <div class="w-full m-2 md:mx-auto md:w-2/3 z-10">
       <Header></Header>
       <Works></Works>
       <Skills></Skills>
@@ -13,6 +15,7 @@
 import Header from './components/Header/ProfileCard.vue'
 import Works from './components/Works/Works.vue'
 import Skills from './components/Skills/Skills.vue'
+import P5 from 'p5'
 
 export default {
   name: 'App',
@@ -20,7 +23,28 @@ export default {
    Header,
    Works,
    Skills,
-  }
+  },
+  created() {
+    const sketch = s => {
+        let w = window.innerWidth;
+        let h = window.innerHeight;
+
+        s.setup = () => {
+            s.createCanvas(w, h);
+        };
+
+        s.draw = () => {
+            s.background(33, 33, 33);
+            s.ellipse(50, 50, 100, 100);
+
+            
+        };
+
+        
+    }
+
+    new P5(sketch, 'canvas');
+}
 }
 </script>
 
